cf7738045f25dd7e70ce600e5d7218cd24039535
Huu Lam Nguyen <liamnguyen.swe@gmail.com>
2023-10-31 23:02:44 -0400

    Update Question List Page

--- app/questions/list/page.tsx
+++ app/questions/list/page.tsx
@@ -5,6 +5,7 @@ import { getServerSession } from "next-auth";
 import QuestionActions from "./QuestionActions";
 import QuestionsAccordion from "./QuestionsAccordion";
 import { Metadata } from "next";
+import { CalloutMessage } from "@/app/components";
 
 const QuestionsPage = async () => {
   const session = await getServerSession(authOptions);
@@ -15,12 +16,23 @@ const QuestionsPage = async () => {
 
   return (
     <Flex direction="column" gap="3">
-      <Text size="5">Frequently Asked Questions</Text>
+      <Text size="5" color="violet">
+        Frequently Asked Questions
+      </Text>
+      <Text size="2">
+        Official Angelina Nail Supply&apos;s Support Center. You can find a
+        quick solution here.
+      </Text>
+      <Text size="2">
+        Our support emails angelinanailsupply@gmail.com or
+        info@angelinanailsupply.com
+      </Text>
       {session && <QuestionActions />}
       <QuestionsAccordion questions={questions} />
     </Flex>
   );
 };
+
 export const dynamic = "force-dynamic";
 export const metadata: Metadata = {
   title: "Frequently Asked Questions",
